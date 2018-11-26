79f03e935d215d788f9a537259100cf0e742dfc1
guo54329 <380772335@qq.com>
2018-11-26 18:16:41 +0800

    首页---1

--- APP/Conf/config.php
+++ APP/Conf/config.php
@@ -1,5 +1,39 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	//数据库配置参数
+	'DB_HOST' => '127.0.0.1',
+	'DB_USER' => 'root',
+	'DB_PWD' => 'root',
+	'DB_NAME' => 'aisen',
+	'DB_PREFIX' => 'ask_',
+
+	//指定后台下的Tpl下的Public文件夹位置
+	'TMPL_PARSE_STRING' => array(
+		'__PUBLIC__' => __ROOT__.'/Public',
+		'__IMAGES__' => __ROOT__.'/Images',
+	), 
+
+	'URL_HTML_SUFFIX' =>'html',  //去掉伪静态后缀名
+    'SHOW_PAGE_TRACE' =>false,
+    
+	//点语法：只让其解析数组
+    'TMPL_VAR_IDENTIFY' => 'array',
+	//模板路径以下划线连接：控制器_方法名.html
+	//'TMPL_FILE_DEPR' => '_',
+
+	//Apache配置：1、要开启mod_rewrite.so , 2、AllowOverride All 。 在本网站根目录下生成.htaccess。然后配置下面一项
+    'URL_MODEL'=>2,   //1为标准默认模式，即地址里面含有入口文件，2为去掉了入口文件。0为普通模式，即一般的网页访问模式（含有入口文件）。
+
+    'URL_ROUTER_ON'=>ture, //开启路由
+	'URL_ROUTE_RULES' => array( //定义路由规则
+		'index'     => 'Index/index',
+		'calculate'     => 'Index/calculate',
+		'result'    => 'Index/result',
+		'verify'    => 'Index/verify',
+		'modify'    => 'Index/modify',
+		'search'    => 'Index/search',
+		'deleteselect'    => 'Index/deleteselect',
+		'deleteall' => 'Index/deleteall'
+	)
 );
 ?>
---
+++ Temp/Cache/e5b19ddd3d471b21bbda02c5e4185b4b.php
@@ -0,0 +1,116 @@
+<?php if (!defined('THINK_PATH')) exit();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="referrer" content="always">
+  <title>泾川县纪念改革开放40周年</title>
+  <link rel="stylesheet" href="__PUBLIC__/Css/index.css" />
+  <style type="text/css">
+  body {
+  margin:0px;
+  padding: 0px;
+  width:1400px;
+  height:750px;
+  background:url(__PUBLIC__/Img/bg.jpg) no-repeat;
+  background-size:100% auto;
+  background-position: center top;
+  text-align:center;
+}
+.main{
+  height:118px;
+  width:1200px;
+  border:2px solid #E4D2AE;
+  border-radius:8px;
+  margin:auto;
+  position: absolute;
+  top:75%;
+  left: 0;
+  right: 0;
+
+  padding:5px;
+  background-color: #E5D3AF;
+  text-align: center;
+}
+  .thumb{
+    width: 143px;
+    height: 84px;
+    padding: 0px 0px;
+    display: inline-block;
+    border-radius: 4px;
+    border:1px solid #ccc;
+    margin-top:3px;
+  }
+  .word{
+    display: inline-block;
+    width: 140px;
+    height: 82px;
+    font-size:11px; 
+    margin-top:2px; 
+  }
+  img{
+    width: 138px;
+    height: 82px;
+    border:2px solid #eee;
+    border-radius: 4px;
+  }
+  </style>
+</head>
+
+<body>
+    <div class="main">
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="党的建设篇">
+            <img src="__IMAGES__/index/1.jpg" alt="党的建设篇" class="thumbimges">
+          </a>
+          <span class="word">党的建设篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="城乡建设篇">
+            <img src="__IMAGES__/index/2.jpg" alt="城乡建设篇" class="thumbimges">
+          </a>
+          <span class="word">城乡建设篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="工业经济篇">
+            <img src="__IMAGES__/index/3.jpg" alt="工业经济篇" class="thumbimges">
+          </a>
+          <span class="word">工业经济篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="美丽乡村篇">
+            <img src="__IMAGES__/index/4.jpg" alt="美丽乡村篇" class="thumbimges">
+          </a>
+          <span class="word">美丽乡村篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="生态环境篇">
+            <img src="__IMAGES__/index/5.jpg" alt="生态环境篇" class="thumbimges">
+          </a>
+          <span class="word">生态环境篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="文化旅游篇">
+            <img src="__IMAGES__/index/6.jpg" alt="文化旅游篇" class="thumbimges">
+          </a>
+          <span class="word">文化旅游篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="民生事业篇">
+            <img src="__IMAGES__/index/7.jpg" alt="民生事业篇" class="thumbimges">
+          </a>
+          <span class="word">民生事业篇</span>
+        </span>
+        <span class="thumb">
+          <a href="#" class="thumbnail" title="百姓生活篇">
+            <img src="__IMAGES__/index/8.jpg" alt="百姓生活篇" class="thumbimges">
+          </a>
+          <span class="word">百姓生活篇</span>
+        </span>
+    </div>
+</body>
+
+</html>
